457cb1e1d0e144b33e85f051b9ba0659df4b3f6c
RayKr <raykr@buaa.edu.cn>
2023-09-20 15:51:04 +0800

    fix: cannot open in zotero with citekey contains `-` or `_`

--- src/open-zotero.ts
+++ src/open-zotero.ts
@@ -1,23 +1,29 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
 
 function extractCiteKey(editor: vscode.TextEditor): string {
-    if (editor.selection.isEmpty) {
-        const range = editor.document.getWordRangeAtPosition(editor.selection.active);
-        return editor.document.getText(range);
-    } else {
-        return editor.document.getText(new vscode.Range(editor.selection.start, editor.selection.end));
-    }
+  if (editor.selection.isEmpty) {
+    const reg = /([\w\d_-]+)/g
+    const range = editor.document.getWordRangeAtPosition(
+      editor.selection.active,
+      reg
+    )
+    return editor.document.getText(range)
+  } else {
+    return editor.document.getText(
+      new vscode.Range(editor.selection.start, editor.selection.end)
+    )
+  }
 }
 
 export async function openInZotero() {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-        return;
-    }
+  const editor = vscode.window.activeTextEditor
+  if (!editor) {
+    return
+  }
 
-    const citeKey = extractCiteKey(editor);
-    console.log(`Opening ${citeKey} in Zotero`);
+  const citeKey = extractCiteKey(editor)
+  console.log(`Opening ${citeKey} in Zotero`)
 
-    const uri = vscode.Uri.parse(`zotero://select/items/bbt:${citeKey}`);
-    await vscode.env.openExternal(uri);
+  const uri = vscode.Uri.parse(`zotero://select/items/bbt:${citeKey}`)
+  await vscode.env.openExternal(uri)
 }
